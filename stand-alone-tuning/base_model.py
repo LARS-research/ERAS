5d9ca4582f16b9c17c9913ab77d796adfb27f0a7
sdiaa <sdiaa@connect.ust.hk>
2021-04-23 00:02:40 +0800

    Add files via upload

--- stand-alone-tuning/base_model.py
+++ stand-alone-tuning/base_model.py
@@ -420,7 +420,7 @@ class BaseModel(object):
         
         best_mrr = 0
         start = time.time()
-        for epoch in range(self.args.n_stand_epoch):
+        for epoch in range(self.args.n_epoch):
                 
                         
             #self.epoch = epoch
@@ -456,7 +456,7 @@ class BaseModel(object):
                             
             scheduler.step()
                         
-            print("Epoch: %d/%d, Loss=%.2f, Stand Time=%.2f"%(epoch+1, self.args.n_stand_epoch, time.time()-start, epoch_loss/n_train))
+            print("Epoch: %d/%d, Stand Time=%.2f, Loss=%.2f"%(epoch+1, self.args.n_epoch, time.time()-start, epoch_loss/n_train))
             
             if (epoch + 1) % 5 == 0:
                 test, randint = True, None
